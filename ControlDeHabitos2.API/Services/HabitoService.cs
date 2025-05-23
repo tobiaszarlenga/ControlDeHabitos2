e1aff8fb91580145f6cfcb64ee58ccc53deb8cce
tobiaszarlenga <162618439+tobiaszarlenga@users.noreply.github.com>
2025-05-22 21:00:05 -0300

    feat: agrego servicio de habitos, implementando lo que hice en la interfaz

---
+++ ControlDeHabitos2.API/Services/HabitoService.cs
@@ -0,0 +1,47 @@
+﻿using ControlDeHabitos2.API.Interfaces;
+
+namespace ControlDeHabitos2.API.Services
+{
+    public class HabitoService : IHabitoService
+    {
+        private readonly List<Habito> _habitos = new();
+        private int _siguienteId = 1;
+        public List<Habito> ObtenerTodos()
+        {
+            return _habitos;
+        }
+        public Habito? ObtenerPorId(int id)
+        {
+            return _habitos.FirstOrDefault(h => h.Id == id);
+        }
+
+        public void Crear(Habito nuevoHabito)
+        {
+            nuevoHabito.Id = _siguienteId++;
+            nuevoHabito.FechaCreacion = DateTime.Now;
+            _habito.Add(nuevoHabito);
+        }
+        public void Actualizar(Habito habitoActualizado)
+        {
+            var existente = ObtenerPorId(habitoActualizado.Id);
+            if (existente is null) return;
+            existente.Nombre = habitoActualizado.Nombre;
+            existente.Descripcion = habitoActualizado.Descripcion;
+            existente.FechaInicio = habitoActualizado.FechaInicio;
+            existente.FrecuenciaPorSemana = habitoActualizado.FrecuenciaPorSemana;
+            existente.HoraObjetivo = habitoActualizado.HoraObjetivo;
+            existente.EstaActivo = habitoActualizado.EstaActivo;
+            existente.EstaCompletoHoy = habitoActualizado.EstaCompletoHoy;
+            existente.DiasCompletados = habitoActualizado.DiasCompletados;
+            existente.FechaUltimaCompletacion = habitoActualizado.FechaUltimaCompletacion;
+
+        }
+        public void Eliminarj(int id)
+        { 
+            var habito = ObtenerPorId(id);
+            if (habito is not null)
+                _habitos.Remove(habito);
+         
+                }
+    }
+}
