cc9cd9b82d5cd08137340cfe259eee4415d04541
Tobias Zarlenga <tobizarlenga@gmail.com>
2025-05-31 21:13:16 -0300

    feat: creo IUsuarioService y IUsuarioRepository con métodos base feat: implemento lógica de usuario en UsuarioService y UsuarioRepository

--- ControlDeHabitos2.API/Repositories/UsuarioRepository.cs
+++ ControlDeHabitos2.API/Repositories/UsuarioRepository.cs
@@ -1,6 +1,42 @@
-﻿namespace ControlDeHabitos2.API.Repositories
+﻿using ControlDeHabitos2.API.Interfaces;
+using ControlDeHabitos2.API.Models;
+
+namespace ControlDeHabitos2.API.Repositories
 {
-    public class UsuarioRepository
+    public class UsuarioRepository : IUsuarioRepository
     {
+        private readonly List<Usuario> _usuarios = new();
+        private int _siguienteId = 1;
+
+        public void Agregar(Usuario usuario)
+        {
+            usuario.Id = _siguienteId++;
+            _usuarios.Add(usuario);
+        }
+
+        public List<Usuario> ObtenerTodos()
+        {
+            return _usuarios;
+        }
+
+        public Usuario? ObtenerPorId(int id)
+        {
+            return _usuarios.FirstOrDefault(u => u.Id == id);
+        }
+        public void Eliminar(int id)
+        {
+            var usuario = _usuarios.FirstOrDefault(u => u.Id == id);
+            if (usuario != null)
+            {
+                _usuarios.Remove(usuario);
+            }
+        }
+
+        public Usuario? ValidarLogin(string nombre, string contraseña)
+        {
+            return _usuarios.FirstOrDefault(u =>
+                u.Nombre == nombre && u.Contraseña == contraseña);
+        }
+
     }
 }
