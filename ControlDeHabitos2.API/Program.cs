bcf02e7da33030621b389d1096b7f47702e8a572
Tobias Zarlenga <tobizarlenga@gmail.com>
2025-06-21 15:11:00 -0300

    feat: configuro el db context en program.cs

--- ControlDeHabitos2.API/Program.cs
+++ ControlDeHabitos2.API/Program.cs
@@ -1,7 +1,9 @@
+using ControlDeHabitos2.API.Data;
 using ControlDeHabitos2.API.Interfaces;
 using ControlDeHabitos2.API.Repositories;
 using ControlDeHabitos2.API.Services;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -30,6 +32,10 @@ builder.Services.AddSingleton<IHabitoService, HabitoService>();
 builder.Services.AddSingleton<IUsuarioService, UsuarioService>();
 
 var app = builder.Build();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 // Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
